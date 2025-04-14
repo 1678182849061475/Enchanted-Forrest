c9f9d496485921b85b6782877a9a0fd250561d76
Veronika N. Štefanec <veronika.nikolaja@gmail.com>
2025-04-14 23:49:22 +0200

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,27 +2,27 @@ tsx
 import { create } from 'zustand';
 
 interface StoreState {
-  musicPath: string;
-  poppingImagesPath: string;
-  characterModelPath: string;
-  forestEnvironmentPath: string;
-  birdCharactersPath: string;
+  musicPath: string="C:\Users\Nika\Dropbox\APP\APP music";
+  poppingImagesPath: string="C:\Users\Nika\Dropbox\APP\.objects\.popping-images";
+  characterModelPath: string="C:\Users\Nika\Dropbox\APP\.objects\1st_person_character\Data-Lovi_human.obj";
+  forestEnvironmentPath: string="C:\Users\Nika\Dropbox\APP\.objects\.forest_enviroment"
+  birdCharactersPath: string="C:\Users\Nika\Dropbox\APP\.objects\.bird-characters"
 }
 
 interface StoreActions {
-  setMusicPath: (path: string) => void;
-  setPoppingImagesPath: (path: string) => void;
-  setCharacterModelPath: (path: string) => void;
+  setMusicPath: (path: string) => play;
+  setPoppingImagesPath: (path: string) => pop_out;
+  setCharacterModelPath: (path: string) => move;
   setForestEnvironmentPath: (path: string) => void;
-  setBirdCharactersPath: (path: string) => void;
+  setBirdCharactersPath: (path: string) => dance;
 }
 
 export const useStore = create<StoreState & StoreActions>((set) => ({
-  musicPath: '',
-  poppingImagesPath: '',
-  characterModelPath: '',
-  forestEnvironmentPath: '',
-  birdCharactersPath: '',
+  musicPath: 'C:\Users\Nika\Dropbox\APP\APP music',
+  poppingImagesPath: 'C:\Users\Nika\Dropbox\APP\.objects\.popping-images',
+  characterModelPath: 'C:\Users\Nika\Dropbox\APP\.objects\1st_person_character\Data-Lovi_human.obj',
+  forestEnvironmentPath: 'C:\Users\Nika\Dropbox\APP\.objects\.forest_enviroment',
+  birdCharactersPath: 'C:\Users\Nika\Dropbox\APP\.objects\.bird-characters',
   setMusicPath: (path: string) => set({ musicPath: path }),
   setPoppingImagesPath: (path: string) => set({ poppingImagesPath: path }),
   setCharacterModelPath: (path: string) => set({ characterModelPath: path }),
