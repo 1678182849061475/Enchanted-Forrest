34b03f02cf739a303f05d1b7a459795b7140c474
Veronika N. Štefanec <veronika.nikolaja@gmail.com>
2025-04-14 23:28:38 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+tsx
+'use client'
+
+import { Suspense, useEffect } from 'react';
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '../src/components/Environment';
+import { CharacterController } from '../src/components/CharacterController';
+import { GeminiBird } from '../src/components/GeminiBird';
+import { ImagePopups } from '../src/components/ImagePopups';
+import { useKeyboard } from '../src/hooks/useKeyboard';
+import { useStore } from '../src/lib/store';
+import { UserInterface } from '../src/components/UserInterface';
+
+export default function Page() {
+  const { moveForward, moveBackward, moveLeft, moveRight, jump } = useKeyboard();
+  const musicPath = process.env.MUSIC_PATH || '';
+  const poppingImagesPath = process.env.POPPING_IMAGES_PATH || '';
+  const characterModelPath = process.env.CHARACTER_MODEL_PATH || '';
+  const forestEnvironmentPath = process.env.FOREST_ENVIRONMENT_PATH || '';
+  const birdCharactersPath = process.env.BIRD_CHARACTERS_PATH || '';
+
+  useEffect(() => {
+    useStore.setState({ musicPath, poppingImagesPath, characterModelPath, forestEnvironmentPath, birdCharactersPath });
+  }, [musicPath, poppingImagesPath, characterModelPath, forestEnvironmentPath, birdCharactersPath]);
+
+  return (
+    <>
+      <Canvas shadows camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 15] }}>
+        <Suspense fallback={null}>
+          <Environment />
+          <CharacterController
+            moveForward={moveForward}
+            moveBackward={moveBackward}
+            moveLeft={moveLeft}
+            moveRight={moveRight}
+            jump={jump}
+          />
+          <GeminiBird />
+          <ImagePopups />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
+        </Suspense>
+      </Canvas>
+      <UserInterface />
+    </>
+  );
+}
