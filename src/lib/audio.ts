b5e641192909164d9a396bed71d426711f8c9aab
Veronika N. Štefanec <veronika.nikolaja@gmail.com>
2025-04-15 02:18:32 +0200

    Update audio.ts

--- src/lib/audio.ts
+++ src/lib/audio.ts
@@ -1,5 +1,5 @@
 typescript
-export const playAudio = (audioPath: string) => {
-  const audio = new Audio(audioPath);
-  audio.play();
+export const playAudio = (Music_files/U.Z.O. (Uho za Oko) - Strup za podgane - 01 Intro.mp3: 'Enchanted-Forest/Music_files/') => {
+  const audio = new Audio(Music_files/U.Z.O. (Uho za Oko) - Strup za podgane - 01 Intro.mp3);
+  audio.play(Music_files/U.Z.O. (Uho za Oko) - Strup za podgane - 01 Intro.mp3);
 };
